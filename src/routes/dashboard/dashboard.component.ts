abb50fd1e7b8ba9d7bd07e9f727a71107c4aa20e
Gustavo <gustavober98@gmail.com>
2025-02-26 09:17:05 -0500

    feat: added loader to dasboard

--- src/routes/dashboard/dashboard.component.ts
+++ src/routes/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LoaderComponent } from '../../components/loader/loader.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
