2f55c549d75fb256f98935ae9cca1a5235943a01
Gustavo Bernal <gustavober98@gmail.com>
2025-03-18 20:57:21 -0500

    feat: added menu notes

--- src/components/Dasboard/books/books.component.ts
+++ src/components/Dasboard/books/books.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-books',
+  standalone: true,
   imports: [],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss'
--- src/routes/dashboard/dashboard.component.ts
+++ src/routes/dashboard/dashboard.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { LoaderComponent } from '../../components/loader/loader.component';
+import { HttpClient } from '@angular/common/http';
+import { BooksComponent } from '../../components/Dasboard/books/books.component';
+import { ListBooksComponent } from "../../components/list-books/list-books.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [LoaderComponent],
+  imports: [LoaderComponent, BooksComponent, ListBooksComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-  
+  loader: boolean = false
+
+  constructor(private http: HttpClient){}
+
+
+
 }
