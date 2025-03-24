f90e79977745fc9a008d9696b53eba2517e62b9c
Gustavo Bernal <gustavober98@gmail.com>
2025-03-24 11:06:52 -0500

    update: update notes

--- src/components/list-books/list-books.component.ts
+++ src/components/list-books/list-books.component.ts
@@ -2,17 +2,18 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { NotesService } from '../../services/dashboard/notes.service';
 import { INotes } from '../../interfaces/INotes';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-list-books',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './list-books.component.html',
   styleUrl: './list-books.component.scss'
 })
 export class ListBooksComponent {
   // estados  de carga
-  loader: boolean = true
+  loader: boolean = false
 
   // estados de datos
   datos: any = []
@@ -32,6 +33,10 @@ export class ListBooksComponent {
 
   // obtiene notas  de cada libreta 
   getNotes(): void {
+    this.loader = true
+    this.datos = []
+    this.errors = []
+
     if (this.idLibreta && typeof this.idLibreta === 'string') {
       this.service.loadNotes(this.idLibreta).subscribe({
         next: (response) => {
