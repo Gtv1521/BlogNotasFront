5e50ea9cf0afedadb904f27f362676b956738018
Gustavo Bernal <gustavober98@gmail.com>
2025-03-24 20:33:03 -0500

    update: update notes

---
+++ src/services/utils/Auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private userId: string | null = null;
+  private tokenUser: string | null = null;
+
+
+  // inicia los datos de session 
+  setAuth(id: string, token: string): void {
+    this.userId = id;
+    this.tokenUser = token
+  }
+
+  // obtiene el id de usuario que ya esta en la session
+  getUserId(): string | null {
+    return this.userId 
+  }
+
+  // lleva el token de usuario a otra funcion 
+  getToken(): string | null {
+    return this.tokenUser
+  } 
+
+// Limpia y destruye la session 
+  clearUserId(): void {
+    this.userId = null;
+    this.tokenUser = null
+  }
+}
